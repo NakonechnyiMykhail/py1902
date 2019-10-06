32f3f77160bb54be3aee188bb8b860db75aabbb6
Mikhail <mykhail.nakonechnyi@gmail.com>
2019-10-06 11:31:25 +0300

    first commit

---
+++ start/22-09/input.py
@@ -0,0 +1,17 @@
+# программа выводит введенное имя + здоровается
+name1 = 'Mike'
+name2 = "text"
+text = '''
+efwefw
+wef         onnp
+wef     hpjp
+wefg
+
+'''
+
+name = input('Please, enter your name: ')
+print("Hello,", name)
+
+print(text)
+# if name == name2:
+#     sdg
---
+++ start/22-09/name.py
@@ -0,0 +1,8 @@
+name = "Mike"
+#myName
+'''
+asf
+sd
+ads
+'''
+print("Hello,", name)
